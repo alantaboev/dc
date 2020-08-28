cc1714e593e497e1babdffa33b098b7f4940dba9
Alan Taboev <alantaboev@mail.ru>
2020-08-28 14:31:43 +0400

    Update for v1.0.0

--- src/Core.php
+++ src/Core.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Differ\Run;
+namespace Differ\Core;
 
 use Docopt;
 
@@ -14,13 +14,15 @@ function run()
     Usage:
       gendiff (-h|--help)
       gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
     
     Options:
       -h --help                     Show this screen
       -v --version                  Show version
+      --format <fmt>                Report format [default: stylish]
 DOC;
 
-    $args = Docopt::handle($doc, array('version' => 'gendiff 1.0'));
+    $args = Docopt::handle($doc, array('version' => 'gendiff 1.0.0'));
     foreach ($args as $k => $v) {
         echo $k . ': ' . json_encode($v) . PHP_EOL;
     }
