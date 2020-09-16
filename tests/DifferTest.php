09e0a5b10861e8d076f5ccc5c7a067ad7db4bcef
Alan Taboev <alantaboev@mail.ru>
2020-09-16 14:59:24 +0400

    Fix comma

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -9,9 +9,9 @@ class DifferTest extends TestCase
 {
     public function testJsonPretty()
     {
-        $before = __DIR__ . '\fixtures\before.json';
-        $after = __DIR__ . '\fixtures\after.json';
-        $content = file_get_contents(__DIR__ . '\fixtures\expectJsonPretty');
+        $before = __DIR__ . "/fixtures/before.json";
+        $after = __DIR__ . "/fixtures/after.json";
+        $content = file_get_contents(__DIR__ . "/fixtures/expectJsonPretty");
         $expect = str_replace("\r\n", "\n", $content); // Заменяем переносы строк, если работаем в Windows
         $this->assertEquals($expect, genDiff($before, $after, 'pretty'));
     }
