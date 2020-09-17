fcfbe90d2244989e20ae26ac582bd8144c1bfa1d
Alan Taboev <alantaboev@mail.ru>
2020-09-17 08:51:13 +0400

    getLine refactoring

--- src/Formatters/Pretty.php
+++ src/Formatters/Pretty.php
@@ -10,34 +10,36 @@ function render(array $differences)
 
 function getLines(array $tree)
 {
-    $indent = '  '; // Отступ строки (двойной пробел)
-    // Сбор строк в массив
-    $strings = array_reduce($tree, function ($acc, $node) use ($indent) {
-        switch ($node['type']) {
-            case 'unchanged':
-                $value = checkValue($node['value']); // Если булев, то вернет строку, соответствующую значению булева
-                $acc[] = "$indent  {$node['key']}: $value";
-                break;
-            case 'added':
-                $value = checkValue($node['value']);
-                $acc[] = "$indent+ {$node['key']}: $value";
-                break;
-            case 'deleted':
-                $value = checkValue($node['value']);
-                $acc[] = "$indent- {$node['key']}: $value";
-                break;
-            case 'changed':
-                $oldValue = checkValue($node['beforeValue']);
-                $newValue = checkValue($node['afterValue']);
-                $acc[] = "$indent- {$node['key']}: $oldValue";
-                $acc[] = "$indent+ {$node['key']}: $newValue";
-                break;
-            default:
-                throw new \Exception("Unknown action '{$node['type']}'");
-        }
-        return $acc;
-    }, $acc = []);
+    $strings = array_reduce($tree, "Differ\\Formatters\\Pretty\\getLine", $acc = []); // Сбор строк в массив
     return implode("\n", $strings); // Возврат массива в строки с разделителем переноса
+}
+
+function getLine($acc, $node)
+{
+    $indent = '  '; // Отступ строки (двойной пробел)
+    switch ($node['type']) {
+        case 'unchanged':
+            $value = checkValue($node['value']); // Если булев, то вернет строку, соответствующую значению булева
+            $acc[] = "$indent  {$node['key']}: $value";
+            break;
+        case 'added':
+            $value = checkValue($node['value']);
+            $acc[] = "$indent+ {$node['key']}: $value";
+            break;
+        case 'deleted':
+            $value = checkValue($node['value']);
+            $acc[] = "$indent- {$node['key']}: $value";
+            break;
+        case 'changed':
+            $oldValue = checkValue($node['beforeValue']);
+            $newValue = checkValue($node['afterValue']);
+            $acc[] = "$indent- {$node['key']}: $oldValue";
+            $acc[] = "$indent+ {$node['key']}: $newValue";
+            break;
+        default:
+            throw new \Exception("Unknown action '{$node['type']}'");
+    }
+    return $acc;
 }
 
 // Проверка значения ключа. Если булев, то возврат строки, соответствующей значению
--- src/Parser.php
+++ src/Parser.php
@@ -2,10 +2,15 @@
 
 namespace Differ\Parser;
 
+use Symfony\Component\Yaml\Yaml;
+
 function parse(string $content, $format)
 {
     if ($format === 'json') {
         return json_decode($content, true, 512, JSON_THROW_ON_ERROR);
+    } elseif ($format === 'yaml' || $format === 'yml') {
+        return Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
+    } else {
+        throw new \Exception("Invalid format: $format. Data must be in JSON or YAML format");
     }
-    throw new \Exception("Invalid format: $format. Data must be in JSON or YAML format");
 }
